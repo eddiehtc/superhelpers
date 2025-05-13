935753561e83ce8a42235026f27db9e6cde50b44
eddie liu <eddie_liu@htc.com>
2025-05-13 13:32:23 +0800

    feat: add storytelling feature >> draft

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: './',
@@ -8,8 +9,14 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'storytelling/index.html'),
+        game: resolve(__dirname, 'index.html'),
+      },
+    },
   },
   server: {
-    open: true,
+    open: '/storytelling/index.html',
   },
 });
